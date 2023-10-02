4327ca532363c5aa0cd04f8f3c33089838b95fb9
Anesu Kafesu <anesukafesu@gmail.com>
2023-10-02 20:24:12 +0200

    4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,6 +3,6 @@
 -- grants all privileges to user on hbnb_test_db
 -- grants select access to user on performance_schema database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test_user'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test_user'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test_user'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
