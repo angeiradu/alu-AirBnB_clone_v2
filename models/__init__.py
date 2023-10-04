e3d4fd59724bc3be6b678a8aedcca6627428408d
Anesu Kafesu <anesukafesu@gmail.com>
2023-10-04 19:59:33 +0200

    6-fix

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,7 @@ global storage
 storage_type = environ['HBNB_TYPE_STORAGE']
 
 if storage_type == "db":
-    from models.engine.db_engine import DBStorage
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
