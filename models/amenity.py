7b4ebcd42ddb2eddcb9ec9ca237fe8d89223b5a3
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-10-06 14:15:49 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,5 +3,11 @@
 from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
