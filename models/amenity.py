997288275710553c198e6e5fa8495de7361ed910
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-10-06 14:26:38 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,9 +3,5 @@
 from models.base_model import BaseModel
 
 class Amenity(BaseModel)
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
+    """This is the class for Amenity"""
     name = ""
