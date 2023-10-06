b087ecfb5c7865f9bb11409814942c0c2ced340c
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-10-06 14:30:02 -0700

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
-class Amenity(BaseModel)
-    """This is the class for Amenity"""
+class Amenity(BaseModel}
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
     name = ""
+    place_amenities = ""
