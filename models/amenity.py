b354064ea65d679bd5afd9d5d0537a74f32ad59e
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-10-07 07:11:04 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-class Amenity(BaseModel}
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
+
+class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
-    name = ""
-    place_amenities = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   back_populates="amenities")
