3dd6908d2b379a708a46e28ae0d3d69ad9b1781e
Anesu Kafesu <a.kafesu@alustudent.com>
2023-10-08 20:32:50 +0200

    tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,41 +1,59 @@
 #!/usr/bin/python3
-"""
-Contains the class TestConsoleDocs
-"""
+"""Unittest for console.py"""
 
-import console
-import inspect
-import pep8
 import unittest
-HBNBCommand = console.HBNBCommand
+from console import HBNBCommand
+from unittest.mock import patch
+from io import StringIO
+import os
+from models import storage
 
 
-class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
-    def test_pep8_conformance_console(self):
-        """Test that console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+@unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                 "Not testing db storage")
+class TestConsole(unittest.TestCase):
+    """Tests for console.py"""
 
-    def test_pep8_conformance_test_console(self):
-        """Test that tests/test_console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def setUp(self):
+        """Sets up test methods"""
+        self.console = HBNBCommand()
+        self.stdout = StringIO()
+        self.storage = storage
 
-    def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
+    def tearDown(self):
+        del self.stdout
+        del self.storage
 
-    def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
+    def test_create_state(self):
+        """Tests create State command"""
+        with patch('sys.stdout', new=self.stdout):
+            self.console.onecmd("create State")
+        output = self.stdout.getvalue().strip()
+        self.assertTrue(output)
+        self.assertTrue(len(output) == 36)
+
+    def test_create_state_with_input(self):
+        """Tests create State command with input"""
+        with patch('sys.stdout', new=self.stdout):
+            self.console.onecmd("create State name=\"California\"")
+            output1 = self.stdout.getvalue().strip()
+            self.console.onecmd("all State")
+            output2 = self.stdout.getvalue().strip()
+        self.assertIn(output1, output2)
+        self.assertIn('California', output2)
+        self.assertIn("[State]", self.stdout.getvalue())
+
+    def test_create_city_with_input(self):
+        """Tests create City command with input"""
+        with patch('sys.stdout', new=self.stdout):
+            self.console.onecmd("create State name=\"California\"")
+            output1 = self.stdout.getvalue().strip()
+            self.console.onecmd("create City state_id=\"{}\" name=\"{}\""
+                                .format(output1, "San_Francisco"))
+            city_id = self.stdout.getvalue().strip().split('\n')[
+                1].split(' ')[0]
+            self.console.onecmd("all City")
+            output3 = self.stdout.getvalue().strip()
+        self.assertIn(city_id, output3)
+        self.assertIn('San Francisco', output3)
+        self.assertIn("[City] ({})".format(city_id), output3)
