80b99cf991c13d8964a6ece2f9cf6748649d4126
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-10-21 14:16:49 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -25,7 +25,7 @@ class State(BaseModel, Base):
         @property
         def cities(self):
             """getter for list of city instances related to the state"""
-          city_list = []
+           city_list = []
             all_cities = models.storage.all(City)
             for city in all_cities.values():
                 if city.state_id == self.id:
