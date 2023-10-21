8827ab7ab667ce98060ff12594e041f41e3e4051
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-10-21 14:46:10 -0700

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
 from models.city import City
-from models import storage
-from os import environ
+import shlex
 
 
 class State(BaseModel, Base):
-    """State class"""
-    __tablename__ = 'states'
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if environ['HBNB_TYPE_STORAGE'] == 'db':
-        cities = relationship('City', backref='states',
-                              cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            return [city for city in storage.all(City).values()
-                    if city.state_id == self.id]
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
